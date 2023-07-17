9ede0ac4cbc6f3ea074763e4b0820cb78e47a9b0
8me <github@8me.de>
2023-07-17 16:00:23 +0200

    Add doc make and deps

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter, Twitch
+
+makedocs(;
+    modules = [Twitch],
+    authors = "Johannes Schumann",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        assets = ["assets/logo.ico"],
+    ),
+    pages=[
+        # "Introduction" => "index.md",
+        # "API" => "api.md",
+    ],
+    repo="https://github.com/8me/Twitch.jl/blob/{commit}{path}#L{line}",
+    sitename="Twitch.jl",
+)
+
+deploydocs(;
+    repo="github.com/8me/Twitch.jl",
+   )
